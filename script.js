f89bf64c9e5fca4854bd40f74657ec46c766af12
Mike Melchior <mike.melchior@outlook.com>
2023-02-02 13:42:58 -0500

    add script for password val

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 const main = document.querySelector('.main');
 
 
+const passwords = document.querySelectorAll('.password')
+const pword = document.querySelector('#pword');
+const cpword = document.querySelector('#cpword');
+const pwordWarning = document.querySelector('.password-warning');
 
+const invalidPasswordWarning = () => {
+    pword.classList.add('invalid');
+    cpword.classList.add('invalid');
+    pwordWarning.classList.remove('hidden');
+}
+
+const removeInvalidPasswordWarning = () => {
+    pword.classList.remove('invalid');
+    cpword.classList.remove('invalid');
+    pwordWarning.classList.add('hidden');
+}
+
+passwords.forEach((password) => {
+    password.addEventListener('input', () => {
+        if (pword.value != cpword.value) {
+            invalidPasswordWarning();
+        } else {
+            removeInvalidPasswordWarning();
+        }
+    })
+})
+
+
+const x = () => {
+    console.log(pword.value)
+}
